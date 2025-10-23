2906f359ec872633143d5d682b47cb30bccb28d0
devsujalpatel <sujal7455@gmail.com>
2025-10-23 23:45:14 +0530

    Local variable

---
+++ contracts/MyContract.sol
@@ -0,0 +1,12 @@
+pragma solidity ^0.8.0;
+
+contract MyContract {
+    // State Variables
+    // Local Variables
+
+    function getValue() public pure returns (uint) {
+        uint value = 1;
+        return value;
+    }
+
+}
